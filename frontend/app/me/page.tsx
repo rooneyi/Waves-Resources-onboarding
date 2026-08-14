8600a33d9dff271abe022e36cd1b947e49600ca6
rooneyi <22ki129@esisalama.org>
2026-08-14 11:58:33 +0200

    fix(frontend): mark interactive app pages as client components

--- frontend/app/me/page.tsx
+++ frontend/app/me/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, {useEffect, useState} from 'react'
 import { getProfile } from '../../lib/api'
 import { Card } from '../../components/ui/Card'
--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, {useState} from 'react'
 import { Button } from '../../components/ui/Button'
 import { Input } from '../../components/ui/Input'
